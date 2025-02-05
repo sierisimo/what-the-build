a13c3f417fc72cad7f4d2b2b99b17ae452b790f5
Sinuhe Jaime <sierisimo@gmail.com>
2025-02-04 23:11:11 -0600

    Adding shortest intro for shortest idea

---
+++ intro/main.c
@@ -0,0 +1,19 @@
+/**
+* In the begining there was C, actually... C was the modern 
+* language that popularized programming, others came before C,
+* like LISP, COBOL, Fortan, BASIC or Pascal.
+*
+* But C proposed cool stuff, was the pionner on talking directly
+* to the computer but in a human way... well, kind of human.
+*
+* BUT WE ARE NOT HERE FOR C...! We just needed an intro
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Hello C");
+
+  return 0;
+}
