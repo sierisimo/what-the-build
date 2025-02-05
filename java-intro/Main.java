237957784376efc41c9d11b56657fa1bfe48d2dd
Sinuhe Jaime <sierisimo@gmail.com>
2025-02-05 00:39:33 -0600

    JVM Fun

--- java-intro/Main.java
+++ java-intro/Main.java
@@ -1,7 +1,17 @@
 class Main {
   public static void main(String[] args) {
+    sleep();
     Message helloMessage = new Message("Hello Java");
 
     System.out.println(helloMessage);
+    sleep();
+  }
+
+  private static void sleep() {
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
